1263359a700a91f82136de16f84fb6720b80f58b
Seung Bin Choi <rusiaboy@gmail.com>
2016-09-08 02:02:05 +1200

    Added a bit more data like wind and humidity and cleaned up a bit

--- js/function.js
+++ js/function.js
@@ -1,11 +1,17 @@
 //This function will call the API and recieve the JSON output from it. 
 function getResult() {
     $("#CityName").html("Receiving data...");
+    $("#CityTemp").html("...");
+    $("#CityCond").html("...");
+    $("#CityWind").html("...");
+    $("#CityHumid").html("...");
     $.get("http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityInput').val() + "&appid=455f145f98daa9855ea05d16d17817ab", function (data) {
         console.log(data);
-        $("#CityName").html("Selected City: [ " + data.name + " ]");
-        $("#CityTemp").html("Temperature: [ " + (data.main.temp - 273.15).toFixed(2) + "&deg;C" + " ]");
-        $("#CityCond").html("Condition: [ " + data.weather[0].description + " ]");
+        $("#CityName").html("Selected City: [ " + data.name + ", " + data.sys.country + " ]");
+        $("#CityTemp").html("Temperature: [ " + (data.main.temp - 273.15).toFixed(2) + " &deg;C" + " ]");
+        $("#CityCond").html("Condition: [ " + data.weather[0].main + " ]");
+        $("#CityWind").html("Wind Speed: [ " + data.wind.speed + " m/s" + " ]");
+        $("#CityHumid").html("Humidity: [ " + data.main.humidity + " %" + " ]");
     });
 }
 //This function will be called when the 'enter' button is clicked on the html page which will then call the getResult function for the API call
