18f8d4e462112333e0c28970e63060a744f9ccff
Seung Bin Choi <rusiaboy@gmail.com>
2016-09-07 23:25:48 +1200

    Updated the results but API calls not working at all..

---
+++ js/function.ts
@@ -0,0 +1,17 @@
+$('#enter').click(function () {
+    var cityName: string = $('#name').val();
+    getWeather(cityName);
+})
+
+//Starts an API call to OpenWeather and display relevant data
+function getWeather(cityName:string) {
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=455f145f98daa9855ea05d16d17817ab",
+        function (data) {
+            console.log(data);
+            $("#weatherRaw").html(data.name + " " 
+                + Math.round((data.main.temp - 273.15) * 10) / 10
+                + "\xB0C " 
+                + data.weather[0].description);
+        }
+    )
+}
--- js/steam.ts
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#TextBoxId').keypress(function(e){
-      if(e.keyCode==13)
-      $('#linkadd').click();
-    });
-});
-
